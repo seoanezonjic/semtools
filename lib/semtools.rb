699f5690e8ac94046eee36f73a947893a907f9aa
seoanezonjic <seoanezonjic@hotmail.com>
2020-05-27 16:19:19 +0200

    Integrated statistical methods from NetAnalyzer

--- lib/semtools.rb
+++ lib/semtools.rb
@@ -2,6 +2,7 @@ require "semtools/version"
 require "semtools/obo_handler"
 require "semtools/sim_handler"
 require "semtools/ontology"
+require "semtools/math_methods"
 
 module Semtools
   # Your code goes here...
