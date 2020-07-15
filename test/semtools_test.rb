357c5f7f4fbb6d71121e0a8d7ba9b9fa9d00fd50
Fernando Moreno Jabato <fmjabato@uma.es>
2020-07-15 10:56:43 +0200

    Removed unnecessary test from semtools_test. All tests working

--- test/semtools_test.rb
+++ test/semtools_test.rb
@@ -5,7 +5,7 @@ class SemtoolsTest < Minitest::Test
     refute_nil ::Semtools::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
+  # def test_it_does_something_useful
+  #   assert false
+  # end
 end
